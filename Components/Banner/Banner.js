689e38d0578ab4d6bb69745226a92b0fecfbae8c
Awlad909 <mdawladbdinfo@gmail.com>
2022-06-13 17:19:38 +0600

    Banner

--- Components/Banner/Banner.js
+++ Components/Banner/Banner.js
@@ -17,7 +17,7 @@ const Banner = () => {
               <span className="text-blue-400 underline">Saas</span>
             </h1>
             <button className="w-full px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-              Start project
+              See More
             </button>
           </div>
         </div>
